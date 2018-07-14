8c25bd859aa22c315adc8557d341ed0b89eebbe0
Niceug <2235422344@qq.com>
2018-07-14 14:46:34 +0800

    添加README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SpEL-demo
 一个SpEL的练习
++ 解析文本
++ 解析对象,实例方法调用
++ 解析集合
++ 在XML中使用SpEL，使用两个配置文件systemProperties和SystemEnvironments
++ 在注解中使用SpEL(使用util:properties标签读取配置文件，赋值给相应对象);
--- src/main/java/com/ltp/spel/SpELApplication.java
+++ src/main/java/com/ltp/spel/SpELApplication.java
@@ -5,15 +5,12 @@ import com.ltp.spel.entity.Person;
 import com.ltp.spel.entity.SystemPropertiesBean;
 import com.ltp.spel.entity.User;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.ParseException;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
-import org.springframework.stereotype.Component;
 
 import java.util.*;
 
@@ -29,27 +26,31 @@ public class SpELApplication {
         String str = parser.parseExpression("'hello world'").getValue(String.class);
         System.out.println(str);
 
+        // 静态方法调用
         Integer integer = parser.parseExpression("T(java.lang.Math).random() * 100").getValue(Integer.class);
         System.out.println(integer);
 
         Boolean bool = parser.parseExpression("true").getValue(Boolean.class);
         System.out.println(bool);
 
+        // 调用字符串中的方法（实例方法调用）
+        String substr = parser.parseExpression("'Hello world'.substring(2)").getValue(String.class);
+        System.out.println(substr);
 
     }
 
-
     /**
-     * 解析对象
+     * 解析对象,实例方法调用
      */
     @Test
     public void test_parseObject(){
-        ExpressionParser parser = new SpelExpressionParser();
 
         User user = new User();
         user.setUsername("tom");
         user.setPassword("123");
-        user.setAge(10);
+        user.setAge(11);
+
+        ExpressionParser parser = new SpelExpressionParser();
         EvaluationContext context = new StandardEvaluationContext(user);
 
         String username = parser.parseExpression("getUsername()").getValue(context, String.class);
@@ -79,11 +80,9 @@ public class SpELApplication {
 
     }
 
-
     /**
      * 在XML中使用SpEL，使用两个配置文件systemProperties和SystemEnvironments
      */
-
     @Test
     public void test_parseXML(){
         ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
@@ -113,6 +112,5 @@ public class SpELApplication {
         DatabaseConfig databaseConfig = context.getBean(DatabaseConfig.class);
         System.out.println(databaseConfig);
     }
-
 }
 
